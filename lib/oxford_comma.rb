a9ca845b4bbf2d1d6c6e39ed1677bf821f30f934
Adam Kapelner <rochlevi@gmail.com>
2017-10-10 22:42:35 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+    
+    if array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    end
+    
+    new_string = array[0]
+    j = 1
+    
+    while j < array.length
+        if j == array.length - 1
+            new_string << ", and #{array[j]}"
+            j += 1
+        else
+        new_string << ", #{array[j]}"
+            j += 1
+        end
+    end
+    return new_string
 end
