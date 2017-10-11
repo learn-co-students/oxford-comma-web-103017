d811dfb8ea462449ebad7103e645d0e14e995e9b
Josh-Stillman <josh.stillman@gmail.com>
2017-10-11 10:45:01 -0700

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return_string = array[0] + " and " + array[1]
+    return return_string
+  else
+    counter = 0
+    return_string = ""
+    while counter < array.size - 1
+      return_string << array[counter] + ", "
+      counter += 1
+    end
+    return_string << "and " + array[counter]
+  end
 
 end
